467a41ed6c6a214f323c83450bb6b1579efc6052
berglund932 <berglund932@gmail.com>
2026-04-01 15:11:23 +0100

    Update _document.js

--- _document.js
+++ _document.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import React from 'react';
+import React from 'react';
 import Document, {Head, Main, NextScript} from 'next/document';
-import {ServerStyleSheet} from 'styled-components';
 import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
